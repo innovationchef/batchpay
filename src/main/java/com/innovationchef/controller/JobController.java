7ca07be484c277c32a10fa5479eddc66027addfb
Ankit Lohani <ankit.lohani.iitkgp@gmail.com>
2020-11-05 13:04:09 +0530

    made the job submittable

--- src/main/java/com/innovationchef/controller/JobController.java
+++ src/main/java/com/innovationchef/controller/JobController.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class JobController {
@@ -30,11 +32,16 @@ public class JobController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     @GetMapping("v1/api/start")
     public Map<String, String> start() throws Exception {
+        String jobId = String.valueOf(System.currentTimeMillis());
         JobParameters parameters = new JobParametersBuilder()
-                .addString(BatchConstant.JOB_ID, String.valueOf(System.currentTimeMillis()))
+                .addString(BatchConstant.JOB_ID, jobId)
                 .addString(BatchConstant.INPUT_FILE, filepath)
                 .toJobParameters();
-        this.launcher.run(job, parameters);
-        return Collections.singletonMap("status", "started");
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "started");
+        response.put("jobId", jobId);
+        ExecutorService es = Executors.newSingleThreadExecutor();
+        es.submit(() -> this.launcher.run(job, parameters));
+        return response;
     }
 }
