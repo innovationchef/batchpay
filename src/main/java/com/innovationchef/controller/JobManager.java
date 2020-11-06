bed209ab7d3cef09b1d9190765c14f0528c6dca1
Ankit Lohani <ankit.lohani.iitkgp@gmail.com>
2020-11-07 03:03:34 +0530

    polish

--- src/main/java/com/innovationchef/controller/JobManager.java
+++ src/main/java/com/innovationchef/controller/JobManager.java
@@ -4,8 +4,9 @@ import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.launch.JobExecutionNotRunningException;
 import org.springframework.batch.core.launch.JobOperator;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
@@ -20,9 +21,6 @@ import java.util.concurrent.ConcurrentHashMap;
 public class JobManager implements ApplicationListener<ContextClosedEvent> {
 
     @Autowired
-    private JobRegistry jobRegistry;
-
-    @Autowired
     private JobRepository jobRepository;
 
     @Autowired
@@ -35,17 +33,20 @@ public class JobManager implements ApplicationListener<ContextClosedEvent> {
     @Override
     public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
         for (Map.Entry<String, JobParameters> jobConfig : jobsMap.entrySet()) {
-            JobParameters parameters = jobConfig.getValue();
-            String jobName = jobConfig.getKey();
-            log.warn("Job {} is in context. Trying to close...", jobName);
-            if (!jobsMap.containsKey(jobName)) continue;
-            JobExecution lastExecution = this.jobRepository.getLastJobExecution(jobName, parameters);
-            if (lastExecution == null || lastExecution.getExitStatus().getExitCode().equals("COMPLETED")) continue;
-            log.warn("Stopping job with Id : {}. Return value {}", lastExecution.getId(), this.jobOperator.stop(lastExecution.getId()));
-            while (!this.jobRepository.getLastJobExecution(jobName, parameters).getExitStatus().getExitCode().equals("STOPPED")) {
-                log.warn(this.jobRepository.getLastJobExecution(jobName, parameters).getExitStatus());
-                Thread.sleep(70L);
-            }
+            log.warn("Job {} is in context. Trying to close...", jobConfig.getKey());
+            shutdownJob(jobConfig.getKey(), jobConfig.getValue());
+        }
+    }
+
+    private void shutdownJob(String jobName, JobParameters parameters)
+            throws NoSuchJobExecutionException, JobExecutionNotRunningException, InterruptedException {
+        if (!this.jobsMap.containsKey(jobName)) return;
+        JobExecution lastExecution = this.jobRepository.getLastJobExecution(jobName, parameters);
+        if (lastExecution == null || lastExecution.getExitStatus().getExitCode().equals("COMPLETED")) return;
+        log.warn("Stopping job with Id : {}. Return value {}", lastExecution.getId(), this.jobOperator.stop(lastExecution.getId()));
+        while (!this.jobRepository.getLastJobExecution(jobName, parameters).getExitStatus().getExitCode().equals("STOPPED")) {
+            log.warn(this.jobRepository.getLastJobExecution(jobName, parameters).getExitStatus());
+            Thread.sleep(70L);
         }
     }
 }
