f357318805feef94eb9f42c276ca227165d91c22
Ankit Lohani <ankit.lohani.iitkgp@gmail.com>
2021-02-17 02:07:17 +0530

    added plugins and extra enforcement via maven plugins

--- src/main/java/com/innovationchef/Application.java
+++ src/main/java/com/innovationchef/Application.java
@@ -2,14 +2,12 @@ package com.innovationchef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableRetry
 @EnableTransactionManagement
-@SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class, ErrorMvcAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
     public static void main(String... args) {
         SpringApplication.run(Application.class);
--- src/test/java/com/innovationchef/ApplicationTests.java
+++ src/test/java/com/innovationchef/ApplicationTests.java
@@ -1,34 +1,5 @@
 package com.innovationchef;
 
-import com.innovationchef.constant.BatchConstant;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.batch.core.Job;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@ActiveProfiles("default")
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
 public class ApplicationTests {
 
-    @Autowired
-    @Qualifier("payJob")
-    private Job payJob;
-
-    @Autowired
-    @Qualifier("custJob")
-    private Job custJob;
-
-    @Test
-    public void test() {
-        Assert.assertNotNull(payJob);
-        Assert.assertNotNull(custJob);
-        Assert.assertEquals(BatchConstant.PAY_JOB_NAME, payJob.getName());
-        Assert.assertEquals(BatchConstant.CUST_JOB_NAME, custJob.getName());
-    }
 }
